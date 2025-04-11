2ba79435df8ecfa603a3d2ce9e3109059f480c13
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-10 23:21:57 -0300

    mas practicas de for

--- ejercicios/piramide.cpp
+++ ejercicios/piramide.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int altura = 5;
+    /*int altura = 5;
     for(int fila = 1; fila <= altura; ++fila){
         for (int espacio = 0; espacio <= altura - fila; ++espacio){
             cout << " ";
@@ -11,6 +11,17 @@ int main(){
             cout << "*";
         }
         cout << endl;
+    }*/
+    cout << "Bien ahora intentaremos hacer una piramide invertida" << endl;
+    int height = 5;
+    for(int row = height; row >= 1; --row){
+        for(int space = 1; space <= height - row; ++space){
+            cout << " ";
+        }
+        for(int aste = 1; aste <= (2 * row -1); ++aste){
+            cout << "*";
+        }
+        cout << endl;
     }
-    
+    return 0;
 }
