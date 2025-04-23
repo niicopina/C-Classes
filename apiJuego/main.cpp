e0583aab63727acc8eb37561516ba8f24ec82caf
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-23 00:04:53 -0300

    API 2 mejoras

--- apiJuego/main.cpp
+++ apiJuego/main.cpp
@@ -11,8 +11,23 @@ int main(){
     cin >> limite_inferior;
     cout <<"Ingresa el numero para el limite superior: \n";
     cin >> limite_superior;
+    //validacion de los limites
+    while(limite_inferior >= limite_superior){
+      cout << "El limite inferior debe ser menor al limite superior, ingreselo denuevo" << endl;
+      cout << "Ingresa el limite inferior: " << endl;
+      cin >> limite_inferior;
+      cout << "Ingrese limite superior: " << endl;
+      cin >> limite_superior;
+    }
+
     cout <<"Ingresa la cantidad de intentos que deseas tener para adivinar: \n";
     cin >> intentos_max;
+
+    while(intentos_max <= 0){
+      cout << "La cantidad de intentos debe ser mayor a 0!" << endl;
+      cout << "Ingrese nuevamente los intentos maximos: " << endl;
+      cin >> intentos_max;
+    }
 
     srand(time(nullptr)); //Llamo a srand de la libreria
 
@@ -23,6 +38,14 @@ int main(){
     for(i = 0; i < intentos_max; ++i){ //un ciclo for con condicionales para ir restando la cantidad de intentos, que ademas le da pistas al usuario
         cout <<"Intento "<< (i + 1) << "/" << intentos_max << " Ingresa un numero para adivinar: ";
         cin >> intento;
+
+        //validacion numero fuera de rango
+        if(intento < limite_inferior || intento > limite_superior){
+         cout << "El numero ingresado esta fuera de rango ("<< limite_inferior<<" - "<< limite_superior<<"). Ingrese nuevamente. \n";
+         --i;
+         continue;
+        }
+
          if (intento < numero_aleatorio){
             cout << "El numero es mayor. Intentalo de nuevo! \n";
          } else if(intento > numero_aleatorio){
