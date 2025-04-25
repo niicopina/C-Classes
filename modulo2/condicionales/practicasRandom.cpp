2ef89c7fdbd3e55db5c3507975442c78a387ebc5
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-24 23:25:39 -0300

    practicando

--- modulo2/condicionales/practicasRandom.cpp
+++ modulo2/condicionales/practicasRandom.cpp
@@ -4,29 +4,43 @@
 using namespace std;
 
 int main(){
-    int limite_inferior, limite_superior, intentos_max;
-    cout << "ingrese el lim inf, el lim sup y los intentos max" << endl;
-    cin >> limite_inferior >> limite_superior >> intentos_max;
-    
+    cout << "Ingrese el minimo y el maximo de la escala para adivinar el numero: ";
+    int minimo, maximo;
+    cin >> minimo;
+    cin >> maximo;
+    if(minimo > maximo){
+        cout << "El minimo debe ser menor al maximo, ingrese nuevamente: " << endl;
+        cin >> minimo;
+        cin >> maximo;
+    }
+    cout << "ahora ingrese la cantidad de intentos que desea tener: ";
+    int intentos_max;
+    cin >> intentos_max;
+    if(intentos_max <= 0){
+        cout << "Debe poner un numero mayor a 0, ingrese nuevamente: ";
+        cin >> intentos_max;
+    }
+
     srand(time(nullptr));
 
-    int numero_aleatorio = limite_inferior + rand() % (limite_superior - limite_inferior -1);
-    int intentos, i;
-    bool adivinado = true;
+    int numero_aleatorio = minimo + rand() % (maximo - minimo -1);
 
-    for(i = 0; i < intentos_max ; i++){
-        cout << "Intentos " << (i+1) << " / " << intentos_max << " Ingresa un numero para comenzar..." << endl;
-        cin >> intentos;
-        if(intentos < numero_aleatorio){
-            cout << "El numero es mayor..." << endl;
-        }else if(intentos > numero_aleatorio){
-            cout << "El numero debe ser menor..." << endl;
+    int intento, i;
+    bool adivinado = false;
+
+    for(i = 0; i < intentos_max; i++){
+        cout << "Intento " << (i + 1) << " / " << intentos_max << endl;
+        cin >> intento;
+        if(intento < numero_aleatorio){
+            cout << "el numero es mayor, intenta de nuevo" << endl;
+        }else if(intento > numero_aleatorio){
+            cout << "el numero es menor..." << endl;
         }else{
-            cout << "ADIVINASTE SON OF A BITCH" << endl;
+            cout << "adivinaste papu" << endl;
             adivinado = true;
         }
     }
     if(!adivinado){
-        cout << " ... D: Perdiste, ahora... MORIRAS " << endl;
+        cout << "game overrrr";
     }
 }
