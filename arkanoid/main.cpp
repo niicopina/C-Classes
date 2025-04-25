23c69a72c64d6f41dba0ca623acd76f564ddb79f
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-24 23:47:25 -0300

    arkanoid

---
+++ arkanoid/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Smfl/include/SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main(){
+    RenderWindow window(VideoMode(800, 600), "Arkanoid");
+}
