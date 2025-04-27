a1b089e81fb9c233904b5343668079043e9d5f42
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-27 19:29:59 -0300

    mejorado

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 int main(){
-    string alumnos[] = {"Nicolas", "Adriana", "Gonzalo", "Daniela", "Felipe"};
-    int notas[] = {5,6,7,3,8};
-    cout << "Se anotaron 5 alumnos al curso: " << endl;
+    string alumnos[] = {"Nico", "pepe", "juanete", "solange", "ricardo"};
+    int notas[] = {9, 5, 6, 2, 8};
+    cout << "Los alumnos que participan de esta clase son: " << endl;
     for(int i=0; i<5; i++){
-        cout << i << "- Nombre: " << alumnos[i] << endl;
-    } 
-    for(int i=0 ; i<5 ; i++){
-        if(notas[i] <= 6){
-            cout << " La nota de " << alumnos[i] <<" fue: "<< notas[i] << " , Desaprueba el examen"<< endl;
-        } else{
-            cout << " La nota de " << alumnos[i] << " fue: " << notas[i]<< " , Aprobado" << endl;
+        cout << "Nombre: " << alumnos[i] << endl;
+    }
+    for(int i= 0; i < 5 ; i++){
+        cout << "La nota de " << alumnos[i] << " fue un: " << notas[i] << endl;
+        if(notas[i] < 6){
+            cout << " Esta desaprobado" << endl;
+        }else{
+            cout << " Curso aprobado" << endl;
         }
     }
+
 }
--- arrays/matriz.cpp
+++ arrays/matriz.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    string matriz[2][3] = 
+/*int main(){
+    string matriz[2][4] = 
     {
-        {"Alfonsina ", "Apolo ", "Chace "} , 
-        {"NoRaza ", "Labrador ", "Manto Negro "}
+        {"Nombre: ", "Alfonsina ", "Apolo ", "Chace "} , 
+        {"Raza: ", "NoRaza ", "Labrador ", "Manto Negro "}
     };
     for(int i=0; i < 2; i++){
-        for(int j= 0; j < 3; j++){
+        for(int j= 0; j < 4; j++){
             cout << matriz[i][j] << " - ";
+        }
+        cout << endl;
+    }
+}*/
+int main(){
+    string alumnos_notas[2][6] = 
+    {
+        {" Nombre: "," Nicolas ", " Adriana ", " Alfonsina ", " Ricky ", " Apolo "},
+        {" Nota: ", "    7 ", "       8 ", "        6 ", "         4 ", "      3 "}
+    };
+    for(int i= 0; i < 2; i++){
+        for(int j = 0; j < 6; j++){
+            cout << alumnos_notas[i][j];
         }
         cout << endl;
     }
