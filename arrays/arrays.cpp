8c445d69d188773a16e5e9c07777a35733f60b88
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-27 18:05:52 -0300

    arrays

---
+++ arrays/arrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string alumnos[5];
+    alumnos[0] = "Daniela Turri";
+    alumnos[1] = "Adriana Soledad";
+    alumnos[2] = "Santiago Torres";
+    alumnos[3] = "Karina Dorner";
+    alumnos[4] = "Nicolas Piña";
+}
