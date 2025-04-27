3330085d530c8e293187dffa61bcd1175cd230b0
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-04-27 18:48:13 -0300

    array alumnos y notas

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 int main(){
-    string alumnos[5];
-    alumnos[0] = "Daniela Turri";
-    alumnos[1] = "Adriana Soledad";
-    alumnos[2] = "Santiago Torres";
-    alumnos[3] = "Karina Dorner";
-    alumnos[4] = "Nicolas Piña";
+    string alumnos[] = {"Nicolas", "Adriana", "Gonzalo", "Daniela", "Felipe"};
+    int notas[] = {5,6,7,3,8};
+    cout << "Se anotaron 5 alumnos al curso: " << endl;
+    for(int i=0; i<5; i++){
+        cout << i << "- Nombre: " << alumnos[i] << endl;
+    } 
+    for(int i=0 ; i<5 ; i++){
+        if(notas[i] <= 6){
+            cout << " La nota de " << alumnos[i] <<" fue: "<< notas[i] << " , Desaprueba el examen"<< endl;
+        } else{
+            cout << " La nota de " << alumnos[i] << " fue: " << notas[i]<< " , Aprobado" << endl;
+        }
+    }
 }
