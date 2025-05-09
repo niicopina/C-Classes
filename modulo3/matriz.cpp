6ebe5c3e0079e19b252dc387b255c8e1ad8a1365
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-08 23:05:43 -0300

    matrices bubble

--- modulo3/matriz.cpp
+++ modulo3/matriz.cpp
@@ -2,25 +2,22 @@
 using namespace std;
 
 int main(){
-    const int CANT_EQUIPOS = 2;
-    const int CANT_ALUMNOS = 5;
+    const int CANT_EQ = 2;
+    const int CANT_AL = 5;
+    string equipos[CANT_EQ][CANT_AL];
 
-    string equipos[CANT_EQUIPOS][CANT_ALUMNOS];
-
-    for(int i = 0; i < CANT_EQUIPOS; i++){
-        cout << "Equipo " << i + 1 << ": "<< endl;
-        for(int j = 0; j < CANT_ALUMNOS; j++){
-            cout << "Ingrese el nombre del alumno " << j + 1 << ": " << endl;
+    for(int i = 0; i < CANT_EQ; i++){
+        cout << "Equipo " << i + 1 << ": " << endl;
+        for(int j = 0; j < CANT_AL; j++){
+            cout << "Ingrese nombre del alumno: " << j + 1 << ": " << endl;
             cin >> equipos[i][j];
         }
     }
-
-    cout << "Listado de equipos" << endl;
-    for(int i = 0; i < CANT_EQUIPOS; i++){
-        cout << "Equipo " << i + 1 << ": ";
-        for(int j = 0; j < CANT_ALUMNOS; j++){
+    cout << "Listado de equipos: "<< endl;
+    for(int i = 0; i < CANT_EQ; i++){
+        cout << "Equipo: " << i + 1 << ": " << endl;
+        for(int j = 0; j < CANT_AL; j++){
             cout << equipos[i][j] << ", ";
         }
-        cout << endl;
     }
 }
