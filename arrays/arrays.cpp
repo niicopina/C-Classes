1bf1f47e6fc8649979073b256753a2141741c96e
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-09 06:40:51 -0300

    Arrays con getline

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    string alumnos[] = {"Nico", "pepe", "juanete", "solange", "ricardo"};
-    int notas[] = {9, 5, 6, 2, 8};
-    cout << "Los alumnos que participan de esta clase son: " << endl;
-    for(int i=0; i<5; i++){
-        cout << "Nombre: " << alumnos[i] << endl;
+    const int CANT_ALUMNOS  = 5;
+    string alumnos[CANT_ALUMNOS];
+    cout << "Ingrese el nombre de cada alumno: " << endl;
+    for(int i = 0; i < CANT_ALUMNOS; i++){
+        getline(cin,alumnos[i]);
     }
-    for(int i= 0; i < 5 ; i++){
-        cout << "La nota de " << alumnos[i] << " fue un: " << notas[i] << endl;
-        if(notas[i] < 6){
-            cout << " Esta desaprobado" << endl;
-        }else{
-            cout << " Curso aprobado" << endl;
-        }
+    cout << "El listado de alumnos queda asi: " << endl;
+    for(int i = 0; i < CANT_ALUMNOS; i++){
+        cout << alumnos[i] << endl;
+    }
+    cout << "Si desea modificar un alumno elija su posicion en la lista: " << endl;
+    int posicion;
+    cin >> posicion;
+    cin.ignore();
+
+    if(posicion >= 0 && posicion < CANT_ALUMNOS){
+        cout << "La posicion elegida es " << posicion << " - Nombre: " << alumnos[posicion] << endl;
+        cout << "Ingrese la modificacion -> ";
+        getline(cin, alumnos[posicion]); 
+    }else{
+        cout << "Posicion invalida, elija entre 0 y 4..." << endl;
+        cin >> posicion;
+    }
+    cout << "La lista modificada queda asi: " << endl;
+    for(int i = 0; i < CANT_ALUMNOS; i++){
+        cout << "Posicion: " << i +1 << " - Nombre: " << alumnos[i] << endl;
     }
 }
