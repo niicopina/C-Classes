8bed5ad65c3478494b19b60bd36f26aae924d938
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-13 20:46:17 -0300

    vectores para listas

--- modulo3/listas.cpp
+++ modulo3/listas.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <list>
+#include <vector>
 using namespace std;
 
 int main(){
+    /* 
     list<int> milista = {10, 9, 8, 4};
-    auto ultimo = milista.end();
+    auto ultimo = milista.end(); // auto es una posicion en la memoria
     milista.insert(ultimo, 20);
     for(int numero : milista){
         cout << numero << endl;
+    }
+    */
+    vector<int> mivector = {5, 1};
+    cout << "cantidad de elementos " << mivector.size() << endl;
+    cout << mivector[1] << endl;
+    mivector.push_back(22);
+    mivector.insert(mivector.begin(), 10);
+    
+    for(int i = 0; i < mivector.size(); i++){
+        cout << mivector[i] << endl;
     }
 }
