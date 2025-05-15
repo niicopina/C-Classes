96982414731beb3ed6afce7e407e3670d09c089f
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-15 20:34:49 -0300

    commit

--- masEjercicios/notasAlumnos.cpp
+++ masEjercicios/notasAlumnos.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int cantidad_alumnos;
+    /*int cantidad_alumnos;
     cout << "Ingrese la cantidad de alumnos que rindieron el examen: ";
     cin >> cantidad_alumnos;
 
@@ -20,5 +20,26 @@ int main(){
     }
     float promedio = (float)suma / cantidad_alumnos;
     cout << "El promedio del curso fue de: " << promedio 
-    << ". La nota mas alta fue: " << nota_max << endl;
+    << ". La nota mas alta fue: " << nota_max << endl;*/
+    
+    const int max_num = 6;
+    int numeros[max_num];
+    cout << "Ingrese 6 numeros enteros: " << endl;
+    for(int i = 0; i < max_num; i++){
+        cin >> numeros[i];
+    }
+    cout << "Ahora los ordenare de manera ascendente: " << endl;
+    for(int i = 0; i < max_num - 1; i++){
+        for(int j = 0; j < max_num - i - 1; j++){
+            if(numeros[j] > numeros[j + 1]){
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+    cout << "El orden es asi:" << endl;
+    for(int i = 0; i < max_num; i++){
+        cout << numeros[i] << " " << endl;
+    }
 }
