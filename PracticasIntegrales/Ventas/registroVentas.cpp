fe8ae026533389deacc8a02027de96ae87494350
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-15 20:56:11 -0300

    Ejercicios Integradores 1 - Ventas

---
+++ PracticasIntegrales/Ventas/registroVentas.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    const int MAX_PROD = 100;
+    int cantidad_productos;
+    cout << "Cuantos productos desea ingresar?" << endl;
+    cin >> cantidad_productos;
+    if(cantidad_productos <= 0 || cantidad_productos > MAX_PROD){
+        cout << "Ingrese una cantidad mayor a 0 y menor a 100: ";
+        cin >> cantidad_productos;
+    }
+
+    string nombres[MAX_PROD];
+    float precios[MAX_PROD];
+    float cant_vendidas[MAX_PROD];
+
+    for(int i = 0; i < cantidad_productos; i++){
+        cout << "Producto: " << i + 1 << endl;
+        cout << "Nombre: ";
+        cin.ignore();
+        getline(cin, nombres[i]);
+        cout << "Precio: ";
+        cin >> precios[i];
+        if(precios[i] <= 0){
+            cout << "El precio debe ser mayor a 0 no?";
+            cin >> precios[i];
+        }
+        cout << "Cantidad vendida: ";
+        cin >> cant_vendidas[i];
+        if(cant_vendidas[i] <= 0){
+            cout << "Algo se tuvo que haber vendido no?";
+            cin >> cant_vendidas[i];
+        }
+    }
+}
