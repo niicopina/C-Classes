5a36a32f7078aef4b92e11de3d498594601e0edf
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-18 20:26:23 -0300

    Clases

---
+++ modulo4/mascotaClass/mascotaClases.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Mascotas{
+    double peso;
+    string nombre;
+    string raza;
+
+    public:
+        void set_peso(int variable){
+            peso = variable;
+        }
+        void set_nombre(string variable){
+            nombre = variable;
+        }
+        void set_raza(string variable){
+            raza = variable;
+        }
+
+        int get_peso(){
+            return peso;
+        }
+        string get_nombre(){
+            return nombre;
+        }
+        void mostrar(){
+            cout << "Nombre: " << nombre << endl;
+            cout << "Raza: " << raza << endl;
+            cout << "Peso: " << peso << endl;
+        }
+};
+int main(){
+    Mascotas miMascota;
+    miMascota.set_nombre("Alfonsina");
+    miMascota.set_raza("P.P");
+    miMascota.set_peso(8.6);
+    miMascota.mostrar();
+
+    cout << "----------------------" << endl;
+
+    Mascotas ramaMascota;
+    ramaMascota.set_nombre("Amapola");
+    ramaMascota.set_raza("P.P");
+    ramaMascota.set_peso(13);
+    ramaMascota.mostrar();
+}
