b33348763dbf3a948136f701c11d29bb6eab984e
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-18 22:41:03 -0300

    modulacion de registroVentas

--- PracticasIntegrales/Ventas/registroVentas.cpp
+++ PracticasIntegrales/Ventas/registroVentas.cpp
@@ -2,7 +2,67 @@
 #include <string>
 using namespace std;
 
+//Constante Global
+const int MAX_PROD = 10;
+
 //Funciones
+void cargarProductos(string nombres[], float precios[], float cant_vendidas[], int cantidad){
+    for(int i = 0; i < cantidad; i++){
+        cout << "Producto " << i + 1 << endl;
+        cout << "Nombre: ";
+        cin.ignore();
+        getline(cin, nombres[i]);
+        do{
+            cout << "Precio: $";
+            cin >> precios[i];
+        }while(precios[i] <= 0);
+        do{
+            cout << "Cantidad vendida: ";
+            cin >> cant_vendidas[i];
+        }while(cant_vendidas[i] <= 0);
+    }
+}
+void mostrarLista(string nombres[], float precios[], float cant_vendidas[], int cantidad){
+    cout << "Antes de continuar revisa la lista: " << endl;
+    for(int i = 0; i < cantidad; i++){
+        cout << "Producto " << i+1 << endl;
+        cout << "Nombre: " << nombres[i]
+             << " | Precio: $" << precios[i]
+             << " | ud. vendidas: " << cant_vendidas[i]
+             << endl;
+    }
+}
+void modificarProducto(string nombres[], float precios[], float cant_vendidas[], int cantidad){
+    int opcion;
+    do{
+        cout << "Desea modificar un producto?" << endl;
+        cout << "1. SI" << endl;
+        cout << "2. NO" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+        if(opcion == 1){
+            int posicion;
+            do{
+                cout << "Para modificarlo ingrese su posicion en la lista (1 a" 
+                     << cantidad << "):";
+                cin >> posicion;
+                posicion--;
+            }while(posicion < 0 || posicion >= cantidad);
+            cout << "La posicion " << posicion + 1
+                 << " corresponde al producto: "
+                 << nombres[posicion] << endl;
+            cout << "Ingrese la modificacion:" << endl;
+            cout << "Nombre: ";
+            cin.ignore();
+            getline(cin, nombres[posicion]);
+            cout << "Precio: $";
+            cin >> precios[posicion];
+            cout << "Unidades vendidas: ";
+            cin >> cant_vendidas[posicion];
+        }
+    }while(opcion != 2);
+}
+
 float recaudacionSemanal(const float precios[], const float cant_vendidas[], int cantidad){
     float total = 0;
     for(int i = 0; i < cantidad; i++){
@@ -39,12 +99,11 @@ void mostrarEstadisticas(float total, const string nombres[], const float cant_v
 }
 
 int main(){
-    const int MAX_PROD = 100;
     int cantidad_productos;
     cout << "Cuantos productos desea ingresar?" << endl;
     cin >> cantidad_productos;
     if(cantidad_productos <= 0 || cantidad_productos > MAX_PROD){
-        cout << "Ingrese una cantidad mayor a 0 y menor a 100: ";
+        cout << "Ingrese una cantidad mayor a 0 y menor a 10: ";
         cin >> cantidad_productos;
     }
 
@@ -52,62 +111,12 @@ int main(){
     float precios[MAX_PROD];
     float cant_vendidas[MAX_PROD];
 
-    for(int i = 0; i < cantidad_productos; i++){
-        cout << "Producto: " << i + 1 << endl;
-        cout << "Nombre: ";
-        cin.ignore();
-        getline(cin, nombres[i]);
-        cout << "Precio: ";
-        cin >> precios[i];
-        if(precios[i] <= 0){
-            cout << "El precio debe ser mayor a 0 no?";
-            cin >> precios[i];
-        }
-        cout << "Cantidad vendida: ";
-        cin >> cant_vendidas[i];
-        if(cant_vendidas[i] <= 0){
-            cout << "Algo se tuvo que haber vendido no?";
-            cin >> cant_vendidas[i];
-        }
-    }
-    cout << "Antes de continuar revisa la lista: " << endl;
-    for(int i = 0; i < cantidad_productos; i++){
-        cout << "Producto " << i+1 << endl;
-        cout << "Nombre: " << nombres[i]
-             << " | Precio: $" << precios[i]
-             << " | ud. vendidas: " << cant_vendidas[i]
-             << endl;
-    }
-    int opcion;
-    do{
-        cout << "Desea modificar un producto?" << endl;
-        cout << "1. SI" << endl;
-        cout << "2. NO" << endl;
-        cout << "Opcion: ";
-        cin >> opcion;
-        if(opcion == 1){
-            int posicion;
-            do{
-                cout << "Para modificarlo ingrese su posicion en la lista (1 a" 
-                     << cantidad_productos << "):";
-                cin >> posicion;
-                posicion--;
-            }while(posicion < 0 || posicion >= cantidad_productos);
-            cout << "La posicion " << posicion + 1
-                 << " corresponde al producto: "
-                 << nombres[posicion] << endl;
-            cout << "Ingrese la modificacion:" << endl;
-            cout << "Nombre: ";
-            cin.ignore();
-            getline(cin, nombres[posicion]);
-            cout << "Precio: $";
-            cin >> precios[posicion];
-            cout << "Unidades vendidas: ";
-            cin >> cant_vendidas[posicion];
-        }
-    }while(opcion != 2);
-    cout << endl;
+    cargarProductos(nombres, precios, cant_vendidas, cantidad_productos);
+    mostrarLista(nombres, precios, cant_vendidas, cantidad_productos);
+    modificarProducto(nombres, precios, cant_vendidas, cantidad_productos);
+
     int indiceMasVendido = mostrarProductos(nombres, precios, cant_vendidas, cantidad_productos);
     float total_recaudado = recaudacionSemanal(precios, cant_vendidas, cantidad_productos);
+    
     mostrarEstadisticas(total_recaudado, nombres, cant_vendidas, indiceMasVendido);
 }
