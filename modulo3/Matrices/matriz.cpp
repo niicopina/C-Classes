9d68d5d7067eb3a517ab8e09aa20a2e81823971b
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-22 22:05:43 -0300

    alumnos array notas promedios

--- modulo3/Matrices/matriz.cpp
+++ modulo3/Matrices/matriz.cpp
@@ -1,46 +1,42 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
-    const int equipos = 3;
     const int alumnos = 3;
-    string listado[equipos][alumnos];
-    for(int i = 0; i < 3; i++){
-        cout << "Equipo " << i + 1 << " - " << endl;
-        cout << "Agregue a los alumnos en cada equipo: ";
-        for(int j = 0; j < 3; j++){
-            cout << j + 1 << ": ";
-            cin >> listado[i][j];
+    const int notas = 3;
+
+    string nombres[alumnos];
+    int calificaciones[alumnos][notas];
+    float promedios[alumnos];
+
+    for(int i = 0; i < alumnos; i++){
+        cout << "Alumno " << i + 1 << ": ";
+        cin >> nombres[i];
+
+        int suma = 0;
+
+        for(int j = 0; j < notas; j++){
+            cout << "Nota de " << nombres[i] << "- " 
+                 << "Examen " << j + 1 << ": ";
+            cin >> calificaciones[i][j];
+            suma += calificaciones[i][j];
+        }
+        promedios[i] = suma / (float)notas;
+    }
+    cout << "Promedios: " << endl;
+    for(int i = 0; i < alumnos; i++){
+        cout << nombres[i] << ": " << promedios[i] << endl;
+    }
+
+    float mejorPromedio = promedios[0];
+    string mejorAlumno = nombres[0];
+
+    for(int i = 0; i < alumnos; i++){
+        if(promedios[i] > mejorPromedio){
+            mejorPromedio = promedios[i];
+            mejorAlumno = nombres[i];
         }
     }
-    cout << "El listado queda asi: " << endl;
-    for(int i = 0; i < 3; i++){
-        cout << "Equipo " << i+ 1 << " - " << endl;
-        for(int j = 0; j < 3; j++){
-            cout << listado[i][j] << " " << endl;
-        }
-    }
-    cout << "Acceda a un alumno segun su posicion en la matriz: " << endl;
-    cout << "0, 1 o 2: "<< endl;
-    cout << "Equipo: ";
-    int equipo;
-    cin >> equipo;
-    cout << "ahora su posicion: ";
-    int posicion;
-    cin >> posicion;
-    if((posicion < 0 || posicion > 3)||(equipo < 0 || equipo > 3)){
-        cout << "Posicion incorrecta..." << endl;
-    }else{
-        cout << "Selecciono a "<< listado[equipo][posicion] << endl;
-        cout << "Modifique el nombre por uno mejor: ";
-        cin >> listado[equipo][posicion];
-    }
-    cout << "Ahora su lista quedo modificada asi:" << endl;
-    for(int i = 0; i < 3; i++){
-        cout << "Equipo " << i + 1 << endl;
-        for(int j = 0; j < 3; j++){
-            cout << listado[i][j] << ", " << endl;
-        }
-    }
+    cout << "El alumno con mejor promedio es "<< mejorAlumno
+         << " con " << mejorPromedio << endl;
 }
