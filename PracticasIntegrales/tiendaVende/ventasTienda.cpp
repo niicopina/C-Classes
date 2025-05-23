342ff336f968a558384200de0ace5181194bba3e
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-05-22 22:42:48 -0300

    ventas tienda array

--- PracticasIntegrales/tiendaVende/ventasTienda.cpp
+++ PracticasIntegrales/tiendaVende/ventasTienda.cpp
@@ -19,7 +19,22 @@ int main(){
             cin >> ud_vendidas[i][j];
             suma += ud_vendidas[i][j];
         }
+        total[i] = suma;
     }
-
+    cout << endl;
+    cout << "Total vendido " << endl;
+    for(int i = 0; i < productos; i++){
+        cout << producto[i] << " = " << total[i] << endl;
+    }
+    int totalMayorProducto = total[0];
+    string productoMasVendido = producto[0];
+    for(int i = 0; i < productos; i++){
+        if(total[i] > totalMayorProducto){
+            totalMayorProducto = total[i];
+            productoMasVendido = producto[i];
+        }
+    }
+    cout << "El producto mas vendido fue " << productoMasVendido
+         << " con " << totalMayorProducto << " ud. vendidas" << endl;
 
 }
