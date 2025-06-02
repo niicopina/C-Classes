8882ddb070b4a9b9746df74051cb16cb7de04c4e
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-06-01 21:09:18 -0300

    practucas

--- PracticasIntegrales/bdProductos/registroProd.cpp
+++ PracticasIntegrales/bdProductos/registroProd.cpp
@@ -2,29 +2,27 @@
 #include <string>
 using namespace std;
 
-const int MAX_PROD = 3;
-
-class Productos {
-    string nombre;
-    float precio;
-    int cantidad;
+class Producto{
     public: 
-    void set_nombre(string variable){
-        nombre = variable;
+    string nombre;
+    int precio;
+    int cantidad;
+    void set_nombre(string n){
+        nombre = n;
     }
-    void set_precio(float variable){
-        precio = variable;
+    void set_precio(int p){
+        precio = p;
     }
-    void set_cantidad(int variable){
-        cantidad = variable;
+    void set_cantidad(int c){
+        cantidad = c;
     }
     string get_nombre(){
         return nombre;
     }
-    float get_precio(){
+    int precio(){
         return precio;
     }
-    int get_cantidad(){
+    int cantidad(){
         return cantidad;
     }
     void mostrar(){
@@ -33,40 +31,22 @@ class Productos {
         cout << "Cantidad: " << cantidad << endl;
     }
 };
-int main(){
-    Productos lista_productos[MAX_PROD];
-    int cantidad = 0;
-    string nombre;
-    float precio;
-    int cantidad_prod;
-    char opcion;
-    do{
-        if(cantidad >= MAX_PROD){
-            cout << "se alcanzo la cantidad maxima" << endl;
-            break;
-        }
-        cout << "Ingrese el Producto: ";
-        cin >> nombre;
-        cout << "Precio: $";
-        cin >> precio;
-        cout << "Cantidad: ";
-        cin >> cantidad_prod;
-
-        lista_productos[cantidad].set_nombre(nombre);
-        lista_productos[cantidad].set_precio(precio);
-        lista_productos[cantidad].set_cantidad(cantidad_prod);
-        cantidad++;
-
-        cout << "Agrega otro producto? s/n - ";
+class Poncho : public Producto{
+    void estilo(){
+        cout << "Seleccione el estilo del poncho: " << endl;
+        cout << "1. Federal" << endl;
+        cout << "2. Escudo" << endl;
+        cout << "3. Independencia" << endl;
+        cout << "4. Cordobes" << endl;
+        cout << "Opcion: ";
+        int opcion;
         cin >> opcion;
-    }while(opcion == 's' || opcion == 'S');
-
-    cout << endl;
-    cout << "Lista de productos ingresados" << endl;
-    
-    for(int i = 0; i < cantidad; i++){
-        cout << "Producto " << i + 1 << ": " << endl;
-        lista_productos[i].mostrar();
-        cout << endl;
+        switch(opcion){
+            case 1: cout << "1. Federal" << endl; break;
+            case 2: cout << "2. Escudo" << endl; break;
+            case 3: cout << "3. Independencia" << endl; break;
+            case 4: cout << "4. Cordobes" << endl; break;
+            default: cout << "invalido";
+        }
     }
-}
+};
