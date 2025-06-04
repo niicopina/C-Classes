b8b4c540deb977f3be6cfa4caf9e86044b2b936a
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-06-04 08:16:18 -0300

    mas funciones bool

--- masEjercicios/ejerciciosDeTodo/littleArrayProd.cpp
+++ masEjercicios/ejerciciosDeTodo/littleArrayProd.cpp
@@ -9,6 +9,14 @@ bool numeroRepetido(const int productos[], int numero, int size){
     }
     return false;
 }
+bool estaEnLaLista(const int productos[], int buscado, int size){
+    for(int i = 0; i < size; i++){
+        if(buscado == productos[i]){
+            return true;
+        }
+    }
+    return false;
+}
 
 int main(){
     const int MAX = 4;
@@ -31,4 +39,22 @@ int main(){
     for(int i = 0; i < MAX; i++){
         cout << "Codigo #" << i+1 << ": " << productos[i] << endl;
     }
+    int opcion;
+    cout << "Desea saber si un codigo ya fue cargado?" << endl;
+    do{
+        cout << "1. Verificar codigo" << endl;
+        cout << "2. Salir." << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+        if(opcion == 1){
+            cout << "Ingrese el codigo para buscarlo: ";
+            int buscado;
+            cin >> buscado;
+            if(estaEnLaLista(productos, buscado, MAX)){
+                cout << "Ya existe en la lista" << endl;
+            }else{
+                cout << "No se encuentra en la lista" << endl;
+            }
+        }
+    }while(opcion != 2);
 }
