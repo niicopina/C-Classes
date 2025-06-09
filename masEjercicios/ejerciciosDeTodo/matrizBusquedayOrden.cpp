50ac25e3eb772ca2b0e91f5ddc0fbce50206fbd3
Nicolás Agustín Piña <pinanicolasagustin@gmail.com>
2025-06-09 10:29:50 -0300

    comit

--- masEjercicios/ejerciciosDeTodo/matrizBusquedayOrden.cpp
+++ masEjercicios/ejerciciosDeTodo/matrizBusquedayOrden.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
 
-const int ROW = 4;
-const int COL = 2;
+
+int main(){
+    const int ROW = 4;
+    const int COL = 2;
+
+    //int codigo[ROW];
+    int matriz[ROW][COL]; 
+
+    cout << "Ingrese el codigo de producto y luego su precio" << endl;
+    for(int i = 0; i < ROW; i++){
+        cout << "Producto #" << i+1 << endl;
+        cout << "Codigo: ";
+        cin >> matriz[i][0];
+        cout << "Precio: $";
+        cin >> matriz[i][1]; 
+    }
+    cout << endl;
+    cout << "CODIGO\tPRECIO" << endl;
+    for(int i = 0; i< ROW; i++){
+        for(int j = 0; j < COL; j++){
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
